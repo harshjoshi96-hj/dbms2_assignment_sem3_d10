0ee7e93e30e0e4ef445f5bdd21245272bcac9f17
harshjoshi96-hj <joshihardy91@gmail.com>
2026-09-11 14:49:56 +0530

    Fix Unit 3 P3.4 fetch variables

--- unit 3/P3.4_Books_Price_Greater_Than_500.sql
+++ unit 3/P3.4_Books_Price_Greater_Than_500.sql
@@ -3,11 +3,15 @@ SET SERVEROUTPUT ON;
 DECLARE
     CURSOR c_book IS
         SELECT book_id, title, price FROM book WHERE price > 500;
+    v_id    book.book_id%TYPE;
+    v_title book.title%TYPE;
+    v_price book.price%TYPE;
 BEGIN
     OPEN c_book;
     LOOP
-        FETCH c_book INTO NULL, NULL, NULL;
+        FETCH c_book INTO v_id, v_title, v_price;
         EXIT WHEN c_book%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE(c_book%ROWCOUNT || '. ' || v_id || ' - ' || v_title || ' - Rs.' || v_price);
     END LOOP;
     CLOSE c_book;
 END;
